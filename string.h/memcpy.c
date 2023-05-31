1a0434cf490defa30e54b363ee07d17af7750a3a
itazure <itazure@gmail.com>
2023-05-31 16:56:14 +0800

    add memcpy

---
+++ string.h/memcpy.c
@@ -0,0 +1,15 @@
+void *memcpy(void *dst, const void *src, size_t n) {
+    __CPROVER_precondition(
+        __CPROVER_POINTER_OBJECT(dst) != __CPROVER_POINTER_OBJECT(src) ||
+            ((const char *)src >= (const char *)dst + n) || ((const char *)dst >= (const char *)src + n),
+        "memcpy src/dst overlap");
+    __CPROVER_precondition(src != NULL && __CPROVER_r_ok(src, n), "memcpy source region readable");
+    __CPROVER_precondition(dst != NULL && __CPROVER_w_ok(dst, n), "memcpy destination region writeable");
+
+    if (n > 0) {
+        size_t index;
+        __CPROVER_assume(index < n);
+        ((uint8_t *)dst)[index] = nondet_uint8_t();
+    }
+    return dst;
+}
