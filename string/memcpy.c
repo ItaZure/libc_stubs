d6be5ccaf85d82825bd1a6b463b63b941d0bcef6
itazure <itazure@gmail.com>
2023-06-02 22:15:06 +0800

    change folder name, minor modifications

--- string/memcpy.c
+++ string/memcpy.c
@@ -1,3 +1,7 @@
+/*
+ * memcpy function
+ */ 
+
 void *memcpy(void *dst, const void *src, size_t n) {
     __CPROVER_precondition(
         __CPROVER_POINTER_OBJECT(dst) != __CPROVER_POINTER_OBJECT(src) ||
